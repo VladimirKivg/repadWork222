264b8d94f27df2ca1ea90d2160ba7a10b2c3a3c0
VladimirKing <cheremuha1@gmail.com>
2021-01-15 01:49:09 +0200

    aa

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -2,6 +2,7 @@ import repadWork_interf.Boeing749;
 import repadWork_interf.Mi82;
 import repadWork_interf.Test2;
 import weapon.Ak47;
+import weapon.Makarov;
 import weapon.Rifle;
 import weapon.Test;
 
@@ -13,11 +14,17 @@ public class RepaidWork {
         Test2 test=new Test2();
         test.foo(mi8);
         test.foo(boeing747);
+
+
         Ak47 ak47=new Ak47();
         Test test1=new Test();
         Rifle rifle=new Rifle();
+        Makarov makarov=new Makarov();
 
+        test1.foo(ak47,ak47);
+        test1.foo(makarov,makarov);
         test1.foo(ak47);
+
     }
 
     private static class Mi8 {
--- src/repadWork_interf/Test2.java
+++ src/repadWork_interf/Test2.java
@@ -2,6 +2,7 @@ package repadWork_interf;
 
 public class Test2 {
     public void foo(FlingObject2 flingObject){
-flingObject.fly();
+
+        flingObject.fly();
     }
 }
