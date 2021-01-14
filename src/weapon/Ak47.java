3076b64bc2bc830c49e64ed1a7db06fcc40b628b
VladimirKing <cheremuha1@gmail.com>
2021-01-15 01:50:44 +0200

    gigi

--- src/weapon/Ak47.java
+++ src/weapon/Ak47.java
@@ -1,6 +1,6 @@
 package weapon;
 
-public class Ak47 extends Rifle{
+public class Ak47 extends Rifle implements WorkWeapon{
     @Override
     public void weapon() {
         ak47();
@@ -8,6 +8,12 @@ public class Ak47 extends Rifle{
     }
 
     private void ak47(){
+
         System.out.println("Ak47");
+    }
+
+    @Override
+    public void workWeapon() {
+        System.out.println("це автомат, але дуже зручний");
     }
 }
--- src/weapon/Makarov.java
+++ src/weapon/Makarov.java
@@ -1,4 +1,22 @@
 package weapon;
 
-public class Makarov {
+public class Makarov extends Rifle implements WorkWeapon{
+    @Override
+    public void weapon() {
+        makarov();
+        super.weapon();
+
+    }
+    private void makarov(){
+
+        System.out.println("пістолет Макаров");
+    }
+    @Override
+    public void workWeapon() {
+
+        System.out.println("це пістоле, не автомат звичайно," +
+                " але краще ніж нічого");
+    }
+
+
 }
