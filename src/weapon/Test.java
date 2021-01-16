bf36ac30216b3b947477eda030f3782d81dacb08
VladimirKing <cheremuha1@gmail.com>
2021-01-16 02:45:06 +0200

    uss

--- src/weapon/Test.java
+++ src/weapon/Test.java
@@ -3,13 +3,17 @@ package weapon;
 public class Test {
 
 
+
     public void foo(Weapon tip, WorkWeapon ti) {
         tip.weapon();
+        tip.foo(tip);
         ti.workWeapon();
     }
 
     public   void foo(Weapon tip){
-        tip.weapon();
+
+        tip.foo(tip);
+
 
     }
 
