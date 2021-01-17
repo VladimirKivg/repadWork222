bbd446d3c6cafec864ff7102c4cd12f59691de18
VladimirKing <cheremuha1@gmail.com>
2021-01-17 06:56:25 +0200

    piip

--- src/repadWork_interf/Test22.java
+++ src/repadWork_interf/Test22.java
@@ -1,2 +1,19 @@
-package repadWork_interf;public class Test22 {
+package repadWork_interf;
+
+public class Test22 <T>{
+   private Test2 test2;
+   private T gen;
+
+    public Test22(Test2 test2, T gen) {
+        this.test2 = test2;
+        this.gen = gen;
+    }
+
+    public Test2 getTest2() {
+        return test2;
+    }
+
+    public T getGen() {
+        return gen;
+    }
 }
