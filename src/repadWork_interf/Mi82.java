c237e76cca77967f2a50bcebf4ae40c30a4065c0
VladimirKing <cheremuha1@gmail.com>
2021-01-17 06:53:04 +0200

    pii

--- src/repadWork_interf/Mi82.java
+++ src/repadWork_interf/Mi82.java
@@ -1,6 +1,15 @@
 package repadWork_interf;
 
+import weapon.Ak47;
+
 public class Mi82 extends Helicopter2{
+
+public void flyWeapon(){
+    Ak47 ak47=new Ak47();
+    verticalTakeoff();
+    System.out.println("оснащення:");
+    ak47.weapon();
+}
     @Override
     public void fly() {
         System.out.println("Вертолет Mi8");
