a767f6c91ae42311d5a9bf8b8490b8c22bd9e3c7
VladimirKing <cheremuha1@gmail.com>
2021-01-19 10:52:24 +0200

    m

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -1,40 +1,7 @@
-import repadWork_interf.*;
-import weapon.Ak47;
-import weapon.Makarov;
-import weapon.Rifle;
-import weapon.Test;
-
 public class RepaidWork {
     public static void main(String[] args) {
 
-        Mi82 mi8=new Mi82();
-        Boeing749 boeing747=new Boeing749();
-        Test2 test=new Test2();
 
-        test.foo((FlingObject2) mi8);
-        test.foo((FlingObject2) mi8);
-
-        test.foo(boeing747);
-
-        Ak47 ak47=new Ak47();
-        Test test1=new Test();
-        Rifle rifle=new Rifle();
-        Makarov makarov=new Makarov();
-
-        test1.foo(ak47,ak47);
-        System.out.println();
-        test1.foo(makarov,makarov);
-        System.out.println();
-        test1.foo(ak47);
-
-        Test22<Ak47>test22=new Test22<Ak47>(test,ak47);
-        test22.getTest2().foo((VerticalTakeoff2) mi8);
-        test22.getGen().weapon();
-        test22.getTest2().foo((FlingObject2) mi8);
-        System.out.println("(-:");
-        mi8.flyWeapon();
-        System.out.println(":-)");
-        test22.getTest2();
 
     }
 
