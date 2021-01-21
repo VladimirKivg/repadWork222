f87b9f775e74d30a75dd13e329f77a9ecfbfb742
VladimirKing <cheremuha1@gmail.com>
2021-01-21 20:14:59 +0200

    m

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -1,7 +1,13 @@
+import weapon.Test;
+import weapon.weaponRifle.Ak47;
+
 public class RepaidWork {
     public static void main(String[] args) {
 
-
+        Ak47 ak47=new Ak47();
+ak47.ak47(ak47);
+        System.out.println();
+        Test test = new Test();
 
     }
 
