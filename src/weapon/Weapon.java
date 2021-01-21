535fe9138b45f8b71317b28687b14b6079f30ebc
VladimirKing <cheremuha1@gmail.com>
2021-01-22 00:26:58 +0200

    prosto

--- src/weapon/Weapon.java
+++ src/weapon/Weapon.java
@@ -1,9 +1,10 @@
 package weapon;
-
+// повторити то що з Ак47 з макаровим
+// капец тут матрешка вийшла
 public abstract class Weapon {
 public abstract void weapon();
 public void foo(Weapon sss){
-    System.out.println("ця збироя");
+    System.out.println("ця зброя");
     sss.weapon();
 }
     }
