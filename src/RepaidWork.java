a4814e537796c08019a0ed0e236078d2588b4453
VladimirKing <cheremuha1@gmail.com>
2021-01-28 01:08:03 +0200

    p

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -8,14 +8,17 @@ public class RepaidWork {
         Ak47 ak47 = new Ak47();
         ak47.fooAk47(ak47);
         System.out.println();
-        Test test = new Test();// З данної рохкладки тест взагалі можна відалити
-test.fooTest(ak47,ak47);
         Makarov makarov =new Makarov();
-        test.fooTest(makarov);
+        Test <Ak47,Makarov>test = new Test(ak47,makarov);// З данної рохкладки тест взагалі можна відалити
+//test.fooTest(ak47,ak47);
+     //   Makarov makarov =new Makarov();
+      //  test.fooTest(makarov);
         System.out.println();
         makarov.fooWeapon(makarov);
         System.out.println();
         makarov.fooMakarov(makarov);
+test.getT().fooAk47(ak47);// тут класс test єксперементальнмй щось подібне краще проробіти з літальнімі апаратамі
+  test.getG().fooMakarov(makarov);
     }
 
     private static class Mi8 {
