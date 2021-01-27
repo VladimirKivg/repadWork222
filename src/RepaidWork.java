4c4eaf7c1c986be79371baed7fc1dcd368ea75cc
VladimirKing <cheremuha1@gmail.com>
2021-01-27 17:34:37 +0200

    m

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -1,5 +1,6 @@
 import weapon.Test;
 import weapon.weaponRifle.Ak47;
+import weapon.weaponRifle.Makarov;
 
 public class RepaidWork {
     public static void main(String[] args) {
@@ -7,8 +8,14 @@ public class RepaidWork {
         Ak47 ak47 = new Ak47();
         ak47.fooAk47(ak47);
         System.out.println();
-        Test test = new Test();
-
+        Test test = new Test();// З данної рохкладки тест взагалі можна відалити
+test.fooTest(ak47,ak47);
+        Makarov makarov =new Makarov();
+        test.fooTest(makarov);
+        System.out.println();
+        makarov.fooWeapon(makarov);
+        System.out.println();
+        makarov.fooMakarov(makarov);
     }
 
     private static class Mi8 {
