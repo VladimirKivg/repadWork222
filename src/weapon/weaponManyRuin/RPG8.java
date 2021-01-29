0dd9db2f08e491ed3bb4e21457f59f647cb300dd
VladimirKing <cheremuha1@gmail.com>
2021-01-29 20:54:53 +0200

    mm

--- src/weapon/weaponManyRuin/RPG8.java
+++ src/weapon/weaponManyRuin/RPG8.java
@@ -1,4 +1,19 @@
 package weapon.weaponManyRuin;
 
-public class RPG8 {
+import weapon.typeWeapon.ManyRuin;
+
+public class RPG8 extends ManyRuin {
+    public void fooRPG8 (ManyRuin e){
+        e.weapon();
+e.workWeapon();
+    }
+    @Override
+    public void weapon() {
+        super.weapon();
+    }
+
+    @Override
+    public void workWeapon() {
+        super.workWeapon();
+    }
 }
