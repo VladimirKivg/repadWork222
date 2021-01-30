95443820206e1756bf5e9cc7241fadca0714effc
VladimirKing <cheremuha1@gmail.com>
2021-01-30 20:44:47 +0200

    mm

--- src/weapon/weaponManyRuin/RPG8.java
+++ src/weapon/weaponManyRuin/RPG8.java
@@ -1,19 +1,24 @@
 package weapon.weaponManyRuin;
 
+import weapon.Weapon;
+import weapon.WorkWeapon;
 import weapon.typeWeapon.ManyRuin;
 
-public class RPG8 extends ManyRuin {
-    public void fooRPG8 (ManyRuin e){
-        e.weapon();
-e.workWeapon();
-    }
-    @Override
-    public void weapon() {
-        super.weapon();
+public class RPG8 extends ManyRuin implements WorkWeapon {
+    public void fooRPG8(Weapon e ) {
+        e.fooWeapon(e);
+        System.out.println("RPG8");
+        workWeapon();
     }
 
     @Override
     public void workWeapon() {
-        super.workWeapon();
+        System.out.println("взриває танки нахрін");
+    }
+
+    @Override
+    public void weapon() {
+        super.weapon();
+        System.out.println("їбашить пиздєц");
     }
 }
