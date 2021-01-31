814673a58013fd4e720c3f877d95364acc0af9e3
VladimirKing <cheremuha1@gmail.com>
2021-01-31 22:04:57 +0200

    mm

--- src/weapon/weaponManyRuin/Tomogawck.java
+++ src/weapon/weaponManyRuin/Tomogawck.java
@@ -1,4 +1,20 @@
 package weapon.weaponManyRuin;
 
-public class Tomogawck {
+import weapon.Weapon;
+import weapon.WorkWeapon;
+import weapon.typeWeapon.ManyRuin;
+
+public  class Tomogawck extends ManyRuin implements WorkWeapon {
+
+    public void fooTomogawck(Weapon e) {
+        e.fooWeapon(e);
+        System.out.println("ракета томогавк");
+        workWeapon();
+    }
+
+    @Override
+    public void workWeapon() {
+        System.out.println("дуже ахрінітільна ракета");
+        System.out.println("знищує цілі на великій вадстані");
+    }
 }
