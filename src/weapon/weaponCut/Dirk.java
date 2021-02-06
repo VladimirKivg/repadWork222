7b628322de94da803d93d5063313c42ba7e1ffb3
VladimirKing <cheremuha1@gmail.com>
2021-02-06 19:04:27 +0200

    mm

--- src/weapon/weaponCut/Dirk.java
+++ src/weapon/weaponCut/Dirk.java
@@ -1,4 +1,11 @@
 package weapon.weaponCut;
 
-public class Dirk {
+import weapon.typeWeapon.Cut;
+
+public class Dirk extends Cut {
+    @Override
+    public void weapon() {
+        super.weapon();
+        System.out.println("нею кидають (митают) для нанесення ран на відстані");
+    }
 }
