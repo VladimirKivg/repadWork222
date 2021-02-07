a03fff8267c60ddca10de9e0c4ae1376e443871e
VladimirKing <cheremuha1@gmail.com>
2021-02-07 02:53:57 +0200

    nn

--- src/weapon/weaponCut/Dirk.java
+++ src/weapon/weaponCut/Dirk.java
@@ -1,11 +1,22 @@
 package weapon.weaponCut;
 
+import weapon.WorkWeapon;
 import weapon.typeWeapon.Cut;
+
+import javax.sql.rowset.WebRowSet;
+
 //добавить и сюда и в остальные колющие интерфейс
-public class Dirk extends Cut {
+public class Dirk extends Cut implements WorkWeapon {
     @Override
     public void weapon() {
         super.weapon();
         System.out.println("нею кидають (митают) для нанесення ран на відстані");
+        workWeapon();
+    }
+
+    @Override
+    public void workWeapon() {
+        System.out.println("використовує спецназ азіатских країн, " +
+                "схоже на зброю нінзя");
     }
 }
