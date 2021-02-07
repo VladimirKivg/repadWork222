8fe7097baca751c1f2b4b54317818dfb4e58c16e
VladimirKing <cheremuha1@gmail.com>
2021-02-07 02:55:24 +0200

    mm

--- src/weapon/weaponCut/Sword.java
+++ src/weapon/weaponCut/Sword.java
@@ -1,10 +1,15 @@
 package weapon.weaponCut;
 
+import weapon.WorkWeapon;
 import weapon.typeWeapon.Cut;
 
 public class Sword extends Cut {
     @Override
     public void weapon() {
         super.weapon();
+        System.out.println("очень острый и удобный меч," +
+                "с смещенным центром тяжости, " +
+                "и хорошо отбалансирован");
     }
+
 }
