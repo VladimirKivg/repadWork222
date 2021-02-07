59e69bfdfec82812a7d69b0e66cce30099246148
VladimirKing <cheremuha1@gmail.com>
2021-02-07 02:54:45 +0200

    nn

--- src/weapon/weaponCut/Knauf.java
+++ src/weapon/weaponCut/Knauf.java
@@ -1,11 +1,20 @@
 package weapon.weaponCut;
 
+import weapon.WorkWeapon;
 import weapon.typeWeapon.Cut;
 
-public class Knauf extends Cut {
+public class Knauf extends Cut implements WorkWeapon{
+    @Override
+    public void workWeapon() {
+        System.out.println("исползует спецназ разных стран" +
+                " как оружие для ближнего боя,");
+        System.out.println("или последняя надежда на выжевание");
+    }
+
     @Override
     public void weapon() {
         super.weapon();
         System.out.println("просто очень хороший нож");
+        workWeapon();
     }
 }
