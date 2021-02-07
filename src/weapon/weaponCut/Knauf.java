b78ac1f94f1f5fcfbe4d05bf35c5cb754e319afc
VladimirKing <cheremuha1@gmail.com>
2021-02-07 02:27:48 +0200

    mm

--- src/weapon/weaponCut/Knauf.java
+++ src/weapon/weaponCut/Knauf.java
@@ -3,5 +3,9 @@ package weapon.weaponCut;
 import weapon.typeWeapon.Cut;
 
 public class Knauf extends Cut {
-
+    @Override
+    public void weapon() {
+        super.weapon();
+        System.out.println("просто очень хороший нож");
+    }
 }
