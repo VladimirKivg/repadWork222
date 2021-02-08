78770698240b0fb7dff973e0a1777f5320a775c4
VladimirKing <cheremuha1@gmail.com>
2021-02-08 17:31:46 +0200

    nn

--- src/repadWork_interf/FlingObject2.java
+++ src/repadWork_interf/FlingObject2.java
@@ -3,4 +3,13 @@ package repadWork_interf;
 public abstract class FlingObject2 {
     private String name;
     public abstract void fly();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
