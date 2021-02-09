b894e2e634b5fc832ce36fb62ea223a1d91dac7b
VladimirKing <cheremuha1@gmail.com>
2021-02-09 19:54:06 +0200

    mm

--- src/repadWork_interf/Aircraft2.java
+++ src/repadWork_interf/Aircraft2.java
@@ -3,5 +3,5 @@ package repadWork_interf;
 import repadWork_interf.FlingObject2;
 
 public abstract class Aircraft2 extends FlingObject2 {
-    //желаем этот класс тоже абстрактным
+    //делаем этот класс тоже абстрактным
 }
