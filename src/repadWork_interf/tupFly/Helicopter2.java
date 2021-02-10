d06d73fe3a471ab41f4107c922f030e35b77c0f1
VladimirKing <cheremuha1@gmail.com>
2021-02-10 04:27:10 +0200

    mm

--- src/repadWork_interf/tupFly/Helicopter2.java
+++ src/repadWork_interf/tupFly/Helicopter2.java
@@ -1,6 +1,9 @@
-package repadWork_interf;
+package repadWork_interf.tupFly;
 
-public abstract class Helicopter2 extends FlingObject2 implements VerticalTakeoff2{//и этот тагже повторно сделаем абстрактным
+import repadWork_interf.FlingObject2;
+import repadWork_interf.VerticalTakeoff2;
+
+public abstract class Helicopter2 extends FlingObject2 implements VerticalTakeoff2 {//и этот тагже повторно сделаем абстрактным
 //Helicopter уже наследуеться от одного класса, а в java множественное наследование запрещено
 /* для ветрикального взлета мы создадим интерфейс так как вертикально может взлетать не только
 * вертолет но и допустим ракета */
