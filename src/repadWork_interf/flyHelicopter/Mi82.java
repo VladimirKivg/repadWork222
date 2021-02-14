37945f0b416891a001ae146c2d8981ed9f6561bd
VladimirKing <cheremuha1@gmail.com>
2021-02-14 04:44:30 +0200

    bb

--- src/repadWork_interf/flyHelicopter/Mi82.java
+++ src/repadWork_interf/flyHelicopter/Mi82.java
@@ -7,11 +7,14 @@ public class Mi82 extends Helicopter2 {
 
 public void flyWeapon(){
     Ak47 ak47=new Ak47();
+
     fly();
     verticalTakeoff();
     System.out.println("оснащення:");
     ak47.weapon();
     Mi82.super.setName("безобидный");
+    System.out.println( Mi82.super.getName());
+
 }
     @Override
     public void fly() {
