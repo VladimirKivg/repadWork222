e7fbea693a3d11d85b4c4146ea9a3ca1bd035cdd
VladimirKing <cheremuha1@gmail.com>
2021-02-14 04:43:27 +0200

    mm

--- src/repadWork_interf/Test3.java
+++ src/repadWork_interf/Test3.java
@@ -1,2 +1,20 @@
-package repadWork_interf;public class Test3 {
+package repadWork_interf;
+
+import repadWork_interf.flyHelicopter.Mi82;
+
+public class Test3 {
+
+
+
+    public static void main(String[] args) {
+        Mi82 mi82=new Mi82();
+        FlingObject2 flingObject2=new Mi82();
+        flingObject2.setName("самолет");
+        flingObject2.fly();
+       // mi82.setName("літаюча хуйня");
+
+        mi82.flyWeapon();
+    }
+
+
 }
