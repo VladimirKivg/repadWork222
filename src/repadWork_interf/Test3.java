b89dd4ba9dd67f8e22402fd83c82456322e60e3c
VladimirKing <cheremuha1@gmail.com>
2021-02-15 03:44:41 +0200

    mm

--- src/repadWork_interf/Test3.java
+++ src/repadWork_interf/Test3.java
@@ -9,7 +9,8 @@ public class Test3 {
     public static void main(String[] args) {
         Mi82 mi82=new Mi82();
         FlingObject2 flingObject2=new Mi82();
-       // flingObject2.setName("самолет");
+        flingObject2.setName("самолет");
+
         flingObject2.fly();
 
         mi82.setName("літаюча хуйня");
