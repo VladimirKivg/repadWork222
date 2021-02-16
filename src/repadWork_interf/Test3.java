6de33a61edc18e7532fd8d34a6e6549351ff59cf
VladimirKing <cheremuha1@gmail.com>
2021-02-16 16:36:14 +0200

    nn

--- src/repadWork_interf/Test3.java
+++ src/repadWork_interf/Test3.java
@@ -5,17 +5,16 @@ import repadWork_interf.flyHelicopter.Mi82;
 public class Test3 {
 
 
-
     public static void main(String[] args) {
-        Mi82 mi82=new Mi82();
-        FlingObject2 flingObject2=new Mi82();
+        Mi82 mi82 = new Mi82();
+        FlingObject2 flingObject2 = new Mi82();
         flingObject2.setName("самолет");
-flingObject2.setSurname("не не самалет, а верталет");
-        flingObject2.fly();
+        flingObject2.setSurname("не не самалет, а верталет");
+      //  flingObject2.fly();
 
         mi82.setName("літаюча хуйня");
         System.out.println(flingObject2.getName());
-        System.out.println(mi82.getName());
+        System.out.println(mi82.getName());//удалить это аж до создания FlingObject2
         mi82.flyWeapon();
 
 
