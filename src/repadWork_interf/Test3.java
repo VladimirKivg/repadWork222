1904d5420583879b0e2e1cd61f018994ec70492d
VladimirKing <cheremuha1@gmail.com>
2021-02-20 20:08:45 +0200

    mm

--- src/repadWork_interf/Test3.java
+++ src/repadWork_interf/Test3.java
@@ -4,16 +4,9 @@ import repadWork_interf.flyHelicopter.Mi82;
 
 public class Test3 {
 
-
     public static void main(String[] args) {
         Mi82 mi82 = new Mi82();
-
-
-
-
         mi82.flyWeapon();
-
-
     }
 
 
