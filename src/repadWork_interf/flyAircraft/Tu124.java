74a548fbdf2bc56a04732a29e3b30dfbbb836256
VladimirKing <cheremuha1@gmail.com>
2021-02-24 05:18:21 +0200

    mm

--- src/repadWork_interf/flyAircraft/Tu124.java
+++ src/repadWork_interf/flyAircraft/Tu124.java
@@ -5,6 +5,6 @@ import repadWork_interf.tupFly.Aircraft2;
 public class Tu124 extends Aircraft2 {
     @Override
     public void fly() {
-
+        System.out.println("маленький блин но очень проворный Tu124");
     }
 }
