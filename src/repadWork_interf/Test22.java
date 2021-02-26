15e9d08ff2d6bb4568d64dd69348b63eaac09703
VladimirKing <cheremuha1@gmail.com>
2021-02-27 01:09:27 +0200

    nn

--- src/repadWork_interf/Test22.java
+++ src/repadWork_interf/Test22.java
@@ -1,8 +1,8 @@
 package repadWork_interf;
 
-public class Test22 <T>{
-   private Test2 test2;
-   private T gen;
+public class Test22<T> {
+    private Test2 test2;
+    private T gen;
 
     public Test22(Test2 test2, T gen) {
         this.test2 = test2;
