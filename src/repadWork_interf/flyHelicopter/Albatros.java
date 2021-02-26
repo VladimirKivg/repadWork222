860e24fc70355ef3a7f854dbc19db257b6337c35
VladimirKing <cheremuha1@gmail.com>
2021-02-26 10:55:15 +0200

    m

--- src/repadWork_interf/flyHelicopter/Albatros.java
+++ src/repadWork_interf/flyHelicopter/Albatros.java
@@ -3,9 +3,10 @@ package repadWork_interf.flyHelicopter;
 import repadWork_interf.tupFly.Helicopter2;
 
 public class Albatros extends Helicopter2 {
-    public void flay(){
+    public void flay() {
 
     }
+
     @Override
     public void fly() {
         System.out.println("Albatros роботящая лошадка ");
