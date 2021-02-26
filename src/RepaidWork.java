03ad2fee853234345b11901a27fea04a157b1db9
VladimirKing <cheremuha1@gmail.com>
2021-02-26 10:52:32 +0200

    m

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -11,27 +11,27 @@ public class RepaidWork {
         Ak47 ak47 = new Ak47();
         ak47.fooAk47(ak47);
         System.out.println();
-        Makarov makarov =new Makarov();
-        Test <Ak47,Makarov>test = new Test(ak47,makarov);// З данної рохкладки тест взагалі можна відалити
+        Makarov makarov = new Makarov();
+        Test<Ak47, Makarov> test = new Test(ak47, makarov);// З данної рохкладки тест взагалі можна відалити
 //test.fooTest(ak47,ak47);
-     //   Makarov makarov =new Makarov();
-      //  test.fooTest(makarov);
+        //   Makarov makarov =new Makarov();
+        //  test.fooTest(makarov);
         System.out.println();
         makarov.fooWeapon(makarov);
         System.out.println();
         makarov.fooMakarov(makarov);
-test.getT().fooAk47(ak47);// тут класс test єксперементальнмй щось подібне краще проробіти з літальнімі апаратамі
+        test.getT().fooAk47(ak47);// тут класс test єксперементальнмй щось подібне краще проробіти з літальнімі апаратамі
         System.out.println("що тут");
-  test.getG().fooMakarov(makarov);
+        test.getG().fooMakarov(makarov);
 
-        RPG8 rpg8=new RPG8();
+        RPG8 rpg8 = new RPG8();
         rpg8.fooRPG8(rpg8);
-        Tomogawck tomogawck=new Tomogawck();
+        Tomogawck tomogawck = new Tomogawck();
         tomogawck.fooTomogawck(tomogawck);
 //        rpg8.weapon();
 //rpg8.fooWeapon(rpg8);
 
-        Sword sword=new Sword();
+        Sword sword = new Sword();
         sword.fooWeapon(sword);
     }
 
