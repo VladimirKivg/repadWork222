b9da65593972fe464969001c1fcb68a0dd1e3168
VladimirKing <cheremuha1@gmail.com>
2021-02-27 01:10:44 +0200

    nn

--- src/repadWork_interf/flyAircraft/Boeing749.java
+++ src/repadWork_interf/flyAircraft/Boeing749.java
@@ -3,14 +3,16 @@ package repadWork_interf.flyAircraft;
 import repadWork_interf.tupFly.Aircraft2;
 
 public class Boeing749 extends Aircraft2 {
-public void flyWor(){}
+    public void flyWor() {
+    }
+
     @Override
     public void fly() {
         System.out.println("я лечу 749");
     }
 
     @Override
-    public void gorizontTakeoff(){
+    public void gorizontTakeoff() {
         System.out.println("горизонтальный взлет");
     }
 }
