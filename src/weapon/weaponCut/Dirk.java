8562b97d8b0222eaf563765ed67fceae718ec214
VladimirKing <cheremuha1@gmail.com>
2021-02-28 15:39:30 +0200

    mm

--- src/weapon/weaponCut/Dirk.java
+++ src/weapon/weaponCut/Dirk.java
@@ -5,7 +5,7 @@ import weapon.typeWeapon.Cut;
 
 import javax.sql.rowset.WebRowSet;
 
-//добавить и сюда и в остальные колющие интерфейс
+//добавить  и сюда и в остальные колющие интерфейс
 public class Dirk extends Cut implements WorkWeapon {
     @Override
     public void weapon() {
