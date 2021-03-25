ccbaa645b10ef9ebc5b538e1158e77eb0ce0136c
VladimirKing <cheremuha1@gmail.com>
2021-03-25 19:49:41 +0200

    mm

--- src/repadWork_interf/flyHelicopter/Albatros.java
+++ src/repadWork_interf/flyHelicopter/Albatros.java
@@ -4,7 +4,7 @@ import repadWork_interf.tupFly.Helicopter2;
 
 public class Albatros extends Helicopter2 {
     public void flay() {// і тут створити цей клас як обєкт, і взагалі всі кінцеві класі мають бути обєктами
-             fly();
+        fly();
         verticalTakeoff();
     }
 
