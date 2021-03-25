6a7c084be7d10d576ff1229d2b3586f569f1768a
VladimirKing <cheremuha1@gmail.com>
2021-03-25 19:50:42 +0200

    mm

--- src/repadWork_interf/flyAircraft/Tu124.java
+++ src/repadWork_interf/flyAircraft/Tu124.java
@@ -3,11 +3,12 @@ package repadWork_interf.flyAircraft;
 import repadWork_interf.tupFly.Aircraft2;
 
 public class Tu124 extends Aircraft2 {
-    public void fluWork(){// спробувати в подальшому створити цей клас як обєк з параметрами і т.д
+    public void fluWork() {// спробувати в подальшому створити цей клас як обєк з параметрами і т.д
         fly();
         gorizontTakeoff();
 
     }
+
     @Override
     public void fly() {
         System.out.println("маленький блин но очень проворный Tu124");
