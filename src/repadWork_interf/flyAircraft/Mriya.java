10f90f7353db927f3d3eb0ebf966b984ad574d49
VladimirKing <cheremuha1@gmail.com>
2021-03-25 21:47:47 +0200

    m

--- src/repadWork_interf/flyAircraft/Mriya.java
+++ src/repadWork_interf/flyAircraft/Mriya.java
@@ -2,7 +2,7 @@ package repadWork_interf.flyAircraft;
 
 import repadWork_interf.tupFly.Aircraft2;
 
-public class Mriya extends Aircraft2 {
+public class    Mriya extends Aircraft2 {
     @Override
     public void fly() {
 
