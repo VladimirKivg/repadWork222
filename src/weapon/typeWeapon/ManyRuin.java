b37fb6cf9969ce9656af697c864f3e30fa0a63ca
VladimirKing <cheremuha1@gmail.com>
2021-03-26 02:39:31 +0200

    mm

--- src/weapon/typeWeapon/ManyRuin.java
+++ src/weapon/typeWeapon/ManyRuin.java
@@ -3,7 +3,7 @@ package weapon.typeWeapon;
 import weapon.Weapon;
 import weapon.WorkWeapon;
 
-public  class  ManyRuin extends Weapon {
+public      class  ManyRuin extends Weapon {
 
     @Override
     public void weapon() {
