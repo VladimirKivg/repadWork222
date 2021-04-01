752f567a72b156c9a18c31f29bce9e6ffad8fdb0
VladimirKing <cheremuha1@gmail.com>
2021-04-01 05:58:42 +0300

    mm

--- src/repadWork_interf/tupFly/Aircraft2.java
+++ src/repadWork_interf/tupFly/Aircraft2.java
@@ -11,7 +11,7 @@ public abstract class Aircraft2 extends FlingObject2 implements GorizontTakeoff 
 
     @Override
     public void gorizontTakeoff() {
-        System.out.println("");
+        System.out.println("горизонтальный взлет");
 
     }
 
