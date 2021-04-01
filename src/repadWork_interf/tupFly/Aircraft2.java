16cc8ec920345644add37145a1b5308bc5f09d82
VladimirKing <cheremuha1@gmail.com>
2021-04-01 05:29:49 +0300

    mm

--- src/repadWork_interf/tupFly/Aircraft2.java
+++ src/repadWork_interf/tupFly/Aircraft2.java
@@ -6,6 +6,6 @@ import repadWork_interf.GorizontTakeoff;
 public abstract class Aircraft2 extends FlingObject2 implements GorizontTakeoff {
     //делаем этот класс тоже абстрактным
     public void max(){
-
+        System.out.println("можно взлетать с разными грузами");
     }
 }
