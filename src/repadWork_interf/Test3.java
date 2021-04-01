c4ea79c1bee1f417ab87d080e34c323ad368c235
VladimirKing <cheremuha1@gmail.com>
2021-04-01 05:33:15 +0300

    mm

--- src/repadWork_interf/Test3.java
+++ src/repadWork_interf/Test3.java
@@ -8,12 +8,13 @@ public class Test3 {
     public static void main(String[] args) {
         Mi82 mi82 = new Mi82();
         mi82.flyWeapon();
-           Test2 test2=new Test2();
+        Test2 test2 = new Test2();
         test2.boeing();
         System.out.println("_______________________");
-        Mriya mriya=new Mriya();
+        Mriya mriya = new Mriya();
         mriya.gorizontTakeoff();
-mi82.verticalTakeoff2();
+        mi82.verticalTakeoff2();
+//        System.out.println("________________________");// tu124
     }
 
 
