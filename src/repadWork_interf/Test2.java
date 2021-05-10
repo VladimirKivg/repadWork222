0826a12f0f770f51c3f4d3da3062d69213d646b3
VladimirKing <cheremuha1@gmail.com>
2021-05-10 14:59:33 +0300

    work

--- src/repadWork_interf/Test2.java
+++ src/repadWork_interf/Test2.java
@@ -1,14 +1,14 @@
 package repadWork_interf;
 
 public class Test2 {
-    public void boeing(){}
+    public void boeing() {
+    }
 
 
     public void foo(FlingObject2 flingObject) {
-            flingObject.fly();
+        flingObject.fly();
 
     }
-
 
 
     public void foo(VerticalTakeoff2 verticalTakeoff2) {
