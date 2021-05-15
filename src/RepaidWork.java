97d02433c83687628b88460fdad842f713e81c04
VladimirKing <cheremuha1@gmail.com>
2021-05-15 11:19:11 +0300

    work

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -13,9 +13,7 @@ public class RepaidWork {
         System.out.println();
         Makarov makarov = new Makarov();
         Test<Ak47, Makarov> test = new Test(ak47, makarov);// З данної рохкладки тест взагалі можна відалити
-//test.fooTest(ak47,ak47);
-        //   Makarov makarov =new Makarov();
-        //  test.fooTest(makarov);
+
         System.out.println();
         makarov.fooWeapon(makarov);
         System.out.println();
